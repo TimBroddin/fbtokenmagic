f88bfc8a6d9e383ed7bf580d7c38f6fb0d976dc6
Tim Broddin <tim.broddin@vrt.be>
2019-01-21 15:54:23 +0100

    added doc and bump version

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ async function getFbToken() {
   if (await fileExists(path)) {
     let contents = await readFile(path, "utf-8");
     return JSON.parse(contents);
+  } else if (process.env.FB_TOKEN_MAGIC) {
+    return JSON.parse(process.env.FB_TOKEN_MAGIC);
   } else {
     throw new Error(`No .fbToken file present at ${path}`);
   }
